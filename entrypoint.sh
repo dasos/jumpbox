7429bcb440675ff15a9035f5bf5767f35a359369
dasos <martin@dasos.com>
2024-12-26 08:58:56 +0000

    Fixing password

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,17 @@ if [ -z "$USERNAME" ]; then
 fi
 
 echo "Adding $USERNAME"
+
+generate_password() {
+    # Generate a random password with a length of 12 characters
+    # You can adjust the length and character set as needed
+    tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 12
+    echo
+}
+PASSWORD=$(generate_password)
+
 adduser $USERNAME --gecos "" --disabled-password --home /home
+chown $USERNAME:$USERNAME /home
 echo "$USERNAME:$PASSWORD" | chpasswd
 
 # Output the username and password
